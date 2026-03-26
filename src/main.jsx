f173dda043d141bae329fad2d0a86404e5539480
suhanraj <suhanr951@gmail.com>
2026-03-26 16:57:53 +0530

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Ai-project/">
+    <BrowserRouter basename="/Ai-project">
       <App />
     </BrowserRouter>
   </StrictMode>
